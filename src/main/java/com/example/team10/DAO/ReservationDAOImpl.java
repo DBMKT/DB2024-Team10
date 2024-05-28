9878f3446ca0158a9bd6a89bb45e97ecbf2b4236
joohee <2170095@ewhain.net>
2024-05-29 04:19:59 +0900

    fix: 세션에서 받아온 유저 정보 지역 변수로 변경

--- src/main/java/com/example/team10/DAO/ReservationDAOImpl.java
+++ src/main/java/com/example/team10/DAO/ReservationDAOImpl.java
@@ -20,26 +20,25 @@ public class ReservationDAOImpl implements ReservationDAO {
         }
     }
 
-    // 세션에 저장된 user 정보 불러오기
-    UserDTO currentUser = SessionManager.getCurrentUser();
-
     // 예약 생성 함수 예시
     @Override
     public void createReservation(ReservationDTO reservation) {
+        // 세션에 저장된 user 정보 불러오기
+        UserDTO currentUser = SessionManager.getCurrentUser();
         if (currentUser == null) {
             System.out.println("로그인된 사용자가 없습니다. 예약을 생성할 수 없습니다.");
             return;
         }
 
-        String query = "INSERT INTO db2024_Reservation (room_id, user_id, reason, people_num, date, period, created_date) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String query = "INSERT INTO db2024_Reservation (room_id, user_id, reason, people_num, reserved_date, reserved_period, created_date) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         try (PreparedStatement statement = conn.prepareStatement(query)) {
             statement.setLong(1, reservation.getRoomId());
             statement.setLong(2, currentUser.getId());
             statement.setString(3, reservation.getReason());
             statement.setInt(4, reservation.getPeopleNum());
-            statement.setDate(5, new java.sql.Date(reservation.getDate().getTime()));
-            statement.setInt(6, reservation.getPeriod());
+            statement.setDate(5, new java.sql.Date(reservation.getReservedDate().getTime()));
+            statement.setInt(6, reservation.getReservedPeriod());
             statement.setDate(7, new java.sql.Date(reservation.getCreatedDate().getTime()));
             statement.executeUpdate();
             System.out.println("예약이 성공적으로 완료되었습니다.");
