6cde65d6c7701a831efbedebffd919c0a7755220
lrenais <bleuing@ewhain.net>
2024-05-31 21:27:01 +0900

    예약 DAO 작업 중

--- src/main/java/com/example/team10/DAO/ReservationDAO.java
+++ src/main/java/com/example/team10/DAO/ReservationDAO.java
@@ -3,5 +3,7 @@ package main.java.com.example.team10.DAO;
 import main.java.com.example.team10.DTO.ReservationDTO;
 
 public interface ReservationDAO {
-    void createReservation(ReservationDTO reservation);
+	ReservationDTO createReservation();
+	
+	public void insertReservation(ReservationDTO reservation);
 }
