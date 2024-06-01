e427cae1df78b5166abeec659b77775c0e98a4e7
joohee <2170095@ewhain.net>
2024-06-02 02:10:53 +0900

    feat: 예약 id 자동 증가, 예약 오류 수정

--- src/main/java/com/example/team10/DAO/ReservationDAOImpl.java
+++ src/main/java/com/example/team10/DAO/ReservationDAOImpl.java
@@ -3,9 +3,12 @@ package main.java.com.example.team10.DAO;
 import main.java.com.example.team10.DTO.AdministratorDTO;
 import main.java.com.example.team10.DTO.ReservationDTO;
 import main.java.com.example.team10.DTO.UserDTO;
+import main.java.com.example.team10.GUI.Reserve;
+import main.java.com.example.team10.GUI.User.UserMyHome;
 import main.java.com.example.team10.util.JdbcUtil;
 import main.java.com.example.team10.util.SessionManager;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -33,85 +36,77 @@ public class ReservationDAOImpl implements ReservationDAO {
     public void createReservation(ReservationDTO reserve) { 
     	//Search에서 정보 넘김-> Reserve에서 수합->createReservation으로 예약 생성->insertReservation에서 sql 처리
     	//예약 인스턴스 생성
-    	ReservationDTO reservation=null;
-    	
-    	//1: reserved_id
-    	int currentId=0;
-    	String findMaxId="SELECT max(reserved_id) FROM db2024_Reservation";
-    	
-    	try(Statement stmt1 = conn.createStatement();){ 
-    		ResultSet rs=stmt1.executeQuery(findMaxId);
-    		if(rs.next()) {
-    			currentId=rs.getInt("reserved_id");
-    		}
-    	}catch (SQLException e) {
-	        e.printStackTrace();
-	        return;
-	    }
-    	
-    	reservation.setReservedId(currentId+1);//현재 예약 테이블의 max 예약 id+1
-    	
-    	//2: room_id
-    	reservation.setRoomId(reserve.getRoomId());//search
-    	
-    	//3: admin_id(어떻게 설정할 것인가?)
-    	int admin_id=99999991+(int)reservation.getReservedId()%4;
-    	reservation.setAdmin_id(admin_id);
-    	
-    	// 세션에 저장된 user 정보 불러오기
+    	ReservationDTO reservation= new ReservationDTO();
+
+        // 세션에 저장된 user 정보 불러오기
         UserDTO currentUser = SessionManager.getCurrentUser();
         if (currentUser == null) {
             System.out.println("로그인된 사용자가 없습니다. 예약을 생성할 수 없습니다.");
             return;
         }
-    	//4: user_id
+
+    	//1: room_id
+    	reservation.setRoomId(reserve.getRoomId());//search
+    	
+    	//2: admin_id(어떻게 설정할 것인가?)
+    	int admin_id=99999991+(int)reservation.getReservedId()%4;
+    	reservation.setAdmin_id(admin_id);
+
+    	//3: user_id
         reservation.setUser_id(currentUser.getId());
-    	//5: user_name
+    	//4: user_name
         reservation.setUserName(currentUser.getName());
-    	//6: reason
+    	//5: reason
         reservation.setReason(reserve.getReason());//reserve
-    	//7: people_num
+    	//6: people_num
         reservation.setPeopleNum(reserve.getPeopleNum());//reserve
-    	//8: reserved_date(예약 날짜)
-        reservation.setReservedDate(reserve.getReservedDate());//search
-    	//9: reserved_period(예약 교시)
+    	//7: reserved_date(예약 날짜)
+        reservation.setReservedDate(new Date(reserve.getReservedDate().getTime()));//search
+    	//8: reserved_period(예약 교시)
         reservation.setReservedPeriod(reserve.getReservedPeriod());//search
-    	//10: created_date(예약 요청 시간)
-        reservation.setCreatedDate(reserve.getCreatedDate());//reserve
+    	//9: created_date(예약 요청 시간)
+        reservation.setCreatedDate(new Date(reserve.getCreatedDate().getTime()));//reserve
         
         insertReservation(reservation);
-        
+
         return;
     }
    
     
     public void insertReservation(ReservationDTO reservation) {//sql에 예약 내역 insert하는 함수(Reserve.java 화면에서 예약 버튼을 누를 시 실행)
-    	String query = "INSERT INTO db2024_Reservation (reserved_id, room_id, admin_id, user_id, user_name, reason, people_num, reserved_date, reserved_period, created_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    	String query = "INSERT INTO db2024_Reservation (room_id, admin_id, user_id, user_name, reason, people_num, reserved_date, reserved_period, created_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try (PreparedStatement statement = conn.prepareStatement(query)) {
-        	//1: reserved_id
-            statement.setLong(1, reservation.getReservedId()); 
-            //2: room_id
-            statement.setLong(2, reservation.getRoomId()); //Reserve.java에서 가져옴
-            //3: admin_id
-            statement.setLong(3, reservation.getAdminId()); //Reserve.java에서 가져옴
-            //4: user_id
-            statement.setInt(4, reservation.getPeopleNum()); 
-            //5: user_name
-            statement.setString(5, reservation.getUserName());
-            //6: reason
-            statement.setString(6, reservation.getReason());
-            //7: people_num
-            statement.setInt(7, reservation.getPeopleNum());            
-            //8: reserved_date
-            statement.setDate(8,(Date) reservation.getReservedDate());
-            //9: reserved_period
-            statement.setInt(9,reservation.getReservedPeriod());
-            //10: created_date
-            statement.setDate(10,(Date)reservation.getCreatedDate());
+            conn.setAutoCommit(false);
+
+            //1: room_id
+            statement.setLong(1, reservation.getRoomId()); //Reserve.java에서 가져옴
+            //2: admin_id
+            statement.setLong(2, reservation.getAdminId()); //Reserve.java에서 가져옴
+            //3: user_id
+            statement.setLong(3, reservation.getUserId());
+            //4: user_name
+            statement.setString(4, reservation.getUserName());
+            //5: reason
+            statement.setString(5, reservation.getReason());
+            //6: people_num
+            statement.setInt(6, reservation.getPeopleNum());
+            //7: reserved_date
+            statement.setDate(7,(Date) reservation.getReservedDate());
+            //8: reserved_period
+            statement.setInt(8,reservation.getReservedPeriod());
+            //9: created_date
+            statement.setDate(9,(Date)reservation.getCreatedDate());
 
             statement.executeUpdate();
+
+            conn.commit();
+
             System.out.println("예약이 성공적으로 완료되었습니다.");
+            // 알림 창 표시
+            JOptionPane.showMessageDialog(null, "예약이 성공적으로 완료되었습니다.", "알림", JOptionPane.INFORMATION_MESSAGE);
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
